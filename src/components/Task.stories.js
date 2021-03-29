87367d0a536ab3aa16faa3e429188fbb2ebc9dbe
CodingCCarpenter <Christineassists@aol.com>
2021-03-29 16:38:41 -0500

    correcting some minor typos in the config file

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,40 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-// import the Task component just created
-import Task from './Task'
+//import the Task component we just created
+import Task from './Task';
 
 export default {
-    component: Task,
-    title: 'Task',
-}
+  component: Task,
+  title: 'Task',
+};
 
-const Template = args => <Task {...args} />
+// Save Task as a template to use in each of our below stories
+const Template = args => <Task {...args} />;
 
-// create the Default story for the Task component
-export const Default = Template.bind({})
+// the Default Story for our component
+export const Default = Template.bind({});
 Default.args = {
-    task: {
-        id: '1',
-        title: 'Test Task',
-        state: 'TASK_INBOX',
-        updatedAt: new Date(2018, 0, 1, 9, 0),
-    },
-}
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2018, 0, 1, 9, 0),
+  },
+};
 
-// create the Pinned story for the Task component
-export const Pinned = Template.bind({})
+// the Pinned Story for our component
+export const Pinned = Template.bind({});
 Pinned.args = {
-    task: {
-        ...Default.args.task,
-        state: 'TASK_PINNED',
-    },
-}
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
 
-// create the Archived story for the Task component
-export const Archived = Template.bind({})
+// the Pinned Story for our component
+export const Archived = Template.bind({});
 Archived.args = {
-    task: {
-        ...Default.args.task,
-        state: 'TASK_ARCHIVED',
-    },
-}
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
